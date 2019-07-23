00268dd2098a3feefe3012d3209a8cf8bb392c7c
Oleksandr Khomenko <homes89@ukr.net>
2019-07-23 22:15:31 +0300

    Fix grid setting

--- src/Board.js
+++ src/Board.js
@@ -4,53 +4,79 @@ import PropTypes from "prop-types";
 import { PLAYER_X, PLAYER_O, SQUARE_WIDTH } from "./constants";
 
 const Board = ({ dims }) => {
-  const [activeSquares, setActiveSquares] = useState([]);
   const [players, setPlayers] = useState({ human: null, computer: null });
   const [gameStarted, setGameStarted] = useState(false);
-  const arr = [...new Array(dims * dims).keys()];
-  const activeSquare = key => activeSquares.find(sq => sq.key === key);
+  const arr = new Array(dims * dims).fill(null);
+  const [grid, setGrid] = useState(arr);
 
-  const humanMove = key => {
-    setActiveSquares(squares => [...squares, { key, type: players.human }]);
+  // TODO deep compare grid
+  useEffect(() => {
     if (checkWin()) {
       declareWinner();
     }
-  };
+  }, [grid]);
 
-  const getEmptySquares = () => {
-    return arr.find(key => !activeSquares.find(square => square.key === key));
+  const humanMove = index => {
+    if (!grid[index]) {
+      setGrid(grid => {
+        const g = grid.concat();
+        g[index] = players.human;
+        return g;
+      });
+    }
+
+    // TODO move computer
   };
 
   const computerMove = () => {
-    const move = 1;
-    if (!activeSquare(move)) {
-      setActiveSquares(squares => [...squares, move]);
-    }
+    const index = 1;
+    setGrid(grid => {
+      grid[index] = players.computer;
+      return grid;
+    });
+  };
 
-    if (checkWin()) {
-      declareWinner();
-    }
+  const getEmptySquares = () => {
+    return grid.filter(square => square === null);
   };
 
   const switchPlayer = player => {
     return player === PLAYER_X ? PLAYER_O : PLAYER_X;
   };
+
   const choosePlayer = option => {
     setPlayers({ human: option, computer: switchPlayer(option) });
     setGameStarted(true);
   };
 
-  const checkWin = () => null;
+  const checkWin = () => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    return null;
+  };
 
   const declareWinner = () => null;
   return gameStarted ? (
     <Container dims={dims}>
-      {arr.map(key => {
-        const active = activeSquare(key);
+      {grid.map((value, index) => {
+        const isActive = value !== null;
 
         return (
-          <Square data-testid="square" key={key} onClick={() => humanMove(key)}>
-            {active && <Marker>{active.type === PLAYER_X ? "X" : "O"}</Marker>}
+          <Square
+            data-testid="square"
+            key={index}
+            onClick={() => humanMove(index)}
+          >
+            {isActive && <Marker>{value === PLAYER_X ? "X" : "O"}</Marker>}
           </Square>
         );
       })}
