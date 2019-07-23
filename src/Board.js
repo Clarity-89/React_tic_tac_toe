cffa1e2b1f32484263651c27f5ab138581aecead
Oleksandr Khomenko <homes89@ukr.net>
2019-07-23 20:58:27 +0300

    Expand board

--- src/Board.js
+++ src/Board.js
@@ -1,31 +1,69 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { SQUARE_WIDTH } from "./constants";
+import { PLAYER_X, PLAYER_O, SQUARE_WIDTH } from "./constants";
 
 const Board = ({ dims }) => {
   const [activeSquares, setActiveSquares] = useState([]);
-  const arr = new Array(dims * dims).fill(1);
+  const [players, setPlayers] = useState({ human: null, computer: null });
+  const [gameStarted, setGameStarted] = useState(false);
+  const arr = [...new Array(dims * dims).keys()];
   const activeSquare = key => activeSquares.find(sq => sq.key === key);
 
-  const onSquareClick = square => {
-    setActiveSquares(squares => [...squares, square]);
+  const playerMove = key => {
+    setActiveSquares(squares => [...squares, { key, type: players.human }]);
   };
-  return (
+
+  const getEmptySquares = () => {
+    return arr.find(key => !activeSquares.find(square => square.key === key));
+  };
+
+  const computerMove = () => {
+    const move = 1;
+    if (!activeSquare(move)) {
+      setActiveSquares(squares => [...squares, move]);
+    }
+
+    if (checkWin()) {
+      declareWinner();
+    }
+  };
+
+  const switchPlayer = player => {
+    return player === PLAYER_X ? PLAYER_O : PLAYER_X;
+  };
+  const choosePlayer = option => {
+    setPlayers({ human: option, computer: switchPlayer(option) });
+    setGameStarted(true);
+  };
+
+  const checkWin = () => null;
+
+  const declareWinner = () => null;
+  return gameStarted ? (
     <Container dims={dims}>
-      {arr.map((dim, key) => {
+      {arr.map(key => {
         const active = activeSquare(key);
+
         return (
           <Square
             data-testid="square"
             key={key}
-            onClick={() => onSquareClick({ key, type: "X" })}
+            onClick={() => playerMove(key)}
           >
-            {active && <Marker>{active.type}</Marker>}
+            {active && <Marker>{active.type === PLAYER_X ? "X" : "O"}</Marker>}
           </Square>
         );
       })}
     </Container>
+  ) : (
+    <StartScreen>
+      <Inner>
+        <ChooseText>Choose your player</ChooseText>
+        <span onClick={() => choosePlayer(PLAYER_X)}>X</span> or{" "}
+        <span onClick={() => choosePlayer(PLAYER_O)}>O</span>
+      </Inner>
+    </StartScreen>
   );
 };
 
@@ -51,6 +89,10 @@ const Square = styled.div`
 const Marker = styled.span`
   font-size: 48px;
 `;
+
+const StartScreen = styled.div``;
+const Inner = styled.div``;
+const ChooseText = styled.p``;
 
 Board.propTypes = {
   dims: PropTypes.number
