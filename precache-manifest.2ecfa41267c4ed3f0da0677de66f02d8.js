ff7b3469bf5d3090c1a4c8cdb52a8c64b6858389
Oleksandr Khomenko <homes89@ukr.net>
2019-07-30 22:48:39 +0300

    Updates

---
+++ precache-manifest.2ecfa41267c4ed3f0da0677de66f02d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a392b80fbb2cc9556cdb1f0d87f83216",
+    "url": "/Clarity-89/React_tic_tac_toe/index.html"
+  },
+  {
+    "revision": "37f26d462f3932f91526",
+    "url": "/Clarity-89/React_tic_tac_toe/static/css/2.712fe445.chunk.css"
+  },
+  {
+    "revision": "b330f8d6f32cc3e38cf7",
+    "url": "/Clarity-89/React_tic_tac_toe/static/css/main.07be6289.chunk.css"
+  },
+  {
+    "revision": "37f26d462f3932f91526",
+    "url": "/Clarity-89/React_tic_tac_toe/static/js/2.cba2b119.chunk.js"
+  },
+  {
+    "revision": "b330f8d6f32cc3e38cf7",
+    "url": "/Clarity-89/React_tic_tac_toe/static/js/main.d97e86b8.chunk.js"
+  },
+  {
+    "revision": "94a14c1b986e86bde798",
+    "url": "/Clarity-89/React_tic_tac_toe/static/js/runtime~main.52ede33d.js"
+  }
+]);
